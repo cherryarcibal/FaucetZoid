420d7b574d95ee0c76ddab3de314475b418ffc28
cherryarcibal <89408387+cherryarcibal@users.noreply.github.com>
2021-09-28 15:26:52 +0800

    Update cfg.php

--- cfg.php
+++ cfg.php
@@ -1,5 +1,5 @@
 <?php
 
-$useragent =" paste disini ";
+$useragent ="Mozilla/5.0 (Linux; Android 11; vivo 1901) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.185 Mobile Safari/537.36";
 
-$cookie =" paste disini ";
+$cookie =" captcha=solvemedia; __cf_bm=MupWnZ7YMBM1LvGkBdSKDoetYXWyQZdvBsL1uKTtMPU-1632813208-0-AeHkbdcGFlpL9Zx8jcNuf7KjYwP6k80PX47clJ8Pv3KrOVm4YeTCvtrPmfd94LKPVwa/v/P/4PLsuEYkZHyY1UJhC7lyzFo7vTbn94qzlvm83uSOE7ukQ4cY/cLDy+NRYw==; 728x90-0=seen; ci_session=5b95ed57dc7f8d03f7fd967f3c495a3d804365cc; 728x90-1=seen; _ga=GA1.2.1828697729.1632813851; _gid=GA1.2.486688861.1632813851; _gat_gtag_UA_70132428_1=1; _data_html=55-1; 728x90-2=seen; _data_cpc=15-1_67-1_190-1; csrf_cookie_name=cfc959ece89dbe676041a3270206c280 ";
